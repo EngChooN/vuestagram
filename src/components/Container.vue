c1aba049788abf87fb6bd02f31840c717a0a4541
Joonyoung Cho <aimzero@vertigogames.com>
2022-08-11 14:16:40 +0900

    1stVuestagramProjectUpload

---
+++ src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Post v-for="(el, index) in boards" :key="index" :elBoards="el" />
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+
+export default {
+  name: "ContainerComponent",
+  components: {
+    Post,
+  },
+  props: {
+    boards: Array,
+  },
+};
+</script>
+
+<style></style>
